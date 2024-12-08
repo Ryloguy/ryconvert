7978690facb8d0d8edc35d94ee92a124dfd7da04
ryloguy <ryloguy87@gmail.com>
2024-12-07 16:58:56 -0800

    MULTICORE SUPPORT :)

--- ryconvert.sh
+++ ryconvert.sh
@@ -2,24 +2,26 @@
 
 printf "\nRy's Lossless iPod Converter :)\n\n"
 
+# Default singlethread variable to 0 to avoid operator error
+singlethread=0
+
 # Process arguments
-while getopts "hdi" opt; do
+while getopts "hs" opt; do
 	case $opt in
 		h) # Help Case
 			printf "%s\n" "By default, this script will detect all FLAC and ALAC files in the current directory and convert them to iPod-friendly ALAC (16-bit 44.1kHz). To change this, use any of the following arguments:
 
 			-h: Help [This screen ;)]
-			-d: Default functionality
-			-i: Convert ALAC files to iPod-friendly ALAC files (16-bit, 44.1kHz)"
+			-s: Run with a single thread"
+			# ADD FLAG TO DISABLE MULTI-CORE
 			printf ""
 			exit
 			;;
-		i)
-			# Add functionality
+
+		s) # Single-thread mode
+			singlethread=1
 			;;
-		d) # Default Case
-			# Add functionality
-			;;
+
 		\?) # Exception Case
 			echo "Invalid argument. Please use -h for help."
 			;;
@@ -49,6 +51,15 @@ if [ $ryconvertflac -eq 0 ] && [ $ryconvertm4a -eq 0 ]; then
 	exit
 fi
 
+# Find and set # of threads
+if [ $singlethread -eq 1 ] ; then
+	echo "Running in single-thread mode"
+	threads=1
+else
+	threads=$(nproc)
+	echo "Found $threads threads"
+fi
+
 # Check for the Originals folder and create one if it does not already exist
 if [ -d "Originals" ]; then
 	echo "An existing Originals folder was found. Please either remove or rename it and try again."
@@ -56,100 +67,112 @@ if [ -d "Originals" ]; then
 	unset ryconvertflac
 	exit
 else
-	echo "Creating Originals folder..."
+	echo "Creating Originals folder...\n"
 	mkdir Originals
 fi
 
-# m4a Check and Conversion
-if [ $ryconvertm4a -eq 1 ]; then
-	echo "Processing .m4a files..."
-	# Add functionality
-	for f in *.m4a;
-	do 
-		echo -e "\nProcessing $f"
-		# Grab file information
-		ryconvertfileinfo=$(ffprobe -v error -show_format -show_streams "$f")
-		# Check for artwork
-		if echo "$ryconvertfileinfo" | grep -q 'mjpeg'; then
-			ffmpeg -i "$f" -an -vcodec copy ".tempart${f%.m4a}.jpg" -loglevel error
-			ryconvertart=".tempart${f%.m4a}.jpg"
-			echo "Found jpg artwork"
-		fi
-		if echo "$ryconvertfileinfo" | grep -q 'png'; then
-			ffmpeg -i "$f" -an -vcodec copy ".tempart${f%.m4a}.png" -loglevel error
-			ryconvertart=".tempart${f%.m4a}.png"
-			echo "Found png artwork"
-		fi
-		# Ensure the current file is actually an ALAC file
-		if echo "$ryconvertfileinfo" | grep -q "alac"; then
-
-			echo "Converting to 16-bit 44100Hz..."
-			ffmpeg -i "$f" -vn -ar 44100 -sample_fmt s16 -acodec flac ".temp${f%.m4a}.flac" -loglevel error
-			mv "$f" "Originals/$f"
-
-			echo "Encoding using ALAC..."
-			ffmpeg -i ".temp${f%.m4a}.flac" -acodec alac "$f" -loglevel error
-			# Attach artwork if applicable
-			if [ "$(echo "$ryconvertart" | wc -c)" -gt 1 ]; then
-				echo "Attaching artwork..."
-				ffmpeg -i "$f" -i "$ryconvertart" -map 0 -map 1 -c copy -disposition:v:0 attached_pic ".temp$f" -loglevel error
-				rm "$f"
-				mv ".temp$f" "$f"
-				rm "$ryconvertart"
-			fi
-			rm ".temp${f%.m4a}.flac"
-			unset ryconvertart
-		else
-			ryconvertalacwarning=1
-			echo "$f doesn't seem to be encoded with ALAC. Skipping."
-		fi
-
-	done
-fi
-
-# FLAC Conversion
-if [ $ryconvertflac -eq 1 ]; then
-	echo "Processing FLAC files..."
-	for f in *.flac;
-	# Do sample rate and bit depth conversion using a temporary .flac file
-	do
-		echo "\nProcessing $f"
-		# Grab file information
-		ryconvertfileinfo=$(ffprobe -v error -show_entries stream=index,codec_name:stream_tags=filename -of default=noprint_wrappers=1:nokey=1 "$f")
-		# Check for artwork
-		if echo "$ryconvertfileinfo" | grep -q 'mjpeg'; then
-			ffmpeg -i "$f" -an -vcodec copy ".tempart${f%.m4a}.jpg" -loglevel error
-			ryconvertart=".tempart${f%.m4a}.jpg"
-			echo "Found jpg artwork"
-		fi
-		if echo "$ryconvertfileinfo" | grep -q 'png'; then
-			ffmpeg -i "$f" -an -vcodec copy ".tempart${f%.m4a}.png" -loglevel error
-			echo "Found png artwork"
-		fi
-
-		echo "Converting to 16-bit 44100Hz..."
-		ffmpeg -i "$f" -vn -ar 44100 -sample_fmt s16 -acodec flac ".temp$f" -loglevel error;
-
-		# Convert the temporary .flac file to ALAC
-		echo "Encoding using ALAC..."
-		ffmpeg -i ".temp$f" -vn -acodec alac "${f%.flac}.m4a" -loglevel error;
+m4a_convert() {
+	# Grab file information
+	ryconvertfileinfo=$(ffprobe -v error -show_format -show_streams "$f")
+	# Check for artwork
+	if echo "$ryconvertfileinfo" | grep -q 'mjpeg'; then
+		ffmpeg -i "$f" -an -vcodec copy ".tempart${f%.m4a}.jpg" -loglevel error
+		ryconvertart=".tempart${f%.m4a}.jpg"
+	fi
+	if echo "$ryconvertfileinfo" | grep -q 'png'; then
+		ffmpeg -i "$f" -an -vcodec copy ".tempart${f%.m4a}.png" -loglevel error
+		ryconvertart=".tempart${f%.m4a}.png"
+	fi
+	# Ensure the current file is actually an ALAC file
+	if echo "$ryconvertfileinfo" | grep -q "alac"; then
+		# Convert to FLAC for bit depth conversion
+		ffmpeg -i "$f" -vn -ar 44100 -sample_fmt s16 -acodec flac ".temp${f%.m4a}.flac" -loglevel error
+		mv "$f" "Originals/$f"
+		# Convert to ALAC
+		ffmpeg -i ".temp${f%.m4a}.flac" -acodec alac "$f" -loglevel error
+		# Attach artwork if possible
 		if [ "$(echo "$ryconvertart" | wc -c)" -gt 1 ]; then
-			echo "Attaching artwork..."
-			ffmpeg -i "${f%.flac}.m4a" -i "$ryconvertart" -map 0 -map 1 -c copy -disposition:v:0 attached_pic ".temp${f%.flac}.m4a" -loglevel error	
-			rm "${f%.flac}.m4a"
-			mv ".temp${f%.flac}.m4a" "${f%.flac}.m4a"
+			ffmpeg -i "$f" -i "$ryconvertart" -map 0 -map 1 -c copy -disposition:v:0 attached_pic ".temp$f" -loglevel error
+			rm "$f"
+			mv ".temp$f" "#f"
 			rm "$ryconvertart"
 		fi
-		# Delete the temporary file
-		rm ".temp$f"
-		# Move the original file to the "Originals" folder
-		mv "$f" "Originals/$f";
+		rm ".temp${f%.m4a}.flac"
 		unset ryconvertart
+		echo "$f converted"
+	else
+		ryconvertalacwarning=1
+		echo "$f doesn't seem to be encoded with ALAC. Skipping."
+	fi
+}
+
+flac_convert() {
+	# Grab file information
+	ryconvertfileinfo=$(ffprobe -v error -show_entries stream=index,codec_name:stream_tags=filename -of default=noprint_wrappers=1:nokey=1 "$f")
+	# Check for artwork
+	if echo "$ryconvertfileinfo" | grep -q 'mjpeg'; then
+		ffmpeg -i "$f" -an -vcodec copy ".tempart${f%.m4a}.jpg" -loglevel error
+		ryconvertart=".tempart${f%.m4a}.jpg"
+	fi
+	if echo "$ryconvertfileinfo" | grep -q 'png'; then
+		ffmpeg -i "$f" -an -vcodec copy ".tempart${f%.m4a}.png" -loglevel error
+		ryconvertart=".tempart${f%.m4a}.jpg"
+	fi
+
+	# Convert to 16-bit 44100Hz
+	ffmpeg -i "$f" -vn -ar 44100 -sample_fmt s16 -acodec flac ".temp$f" -loglevel error
+
+	# Convert temp .flac file to ALAC
+	ffmpeg -i ".temp$f" -vn -acodec alac "${f%.flac}.m4a" -loglevel error
+	
+	# Add artwork if applicable
+	if [ "$(echo "$ryconvertart" | wc -c)" -gt 1 ]; then
+		ffmpeg -i "${f%.flac}.m4a" -i "$ryconvertart" -map 0 -map 1 -c copy -disposition:v:0 attached_pic ".temp${f%.flac}.m4a" -loglevel error
+		rm "${f%.flac}.m4a"
+		mv ".temp${f%.flac}.m4a" "${f%.flac}.m4a"
+		rm "$ryconvertart"
+	fi
+	# Delete temp file
+	rm ".temp$f"
+	# Move original file to "Originals" folder
+	mv "$f" "Originals/$f"
+	unset ryconvertart
+	echo "$f converted from FLAC"
+}
+
+# Export functions for subshells
+export -f m4a_convert
+export -f flac_convert
+
+# m4a conversion loop
+if [ $ryconvertm4a -gt 0 ]; then
+	for f in *.m4a; do
+		m4a_convert "$f" &
+		((count++))
+		# Try and find a way to start a new job as soon as an existing one ends
+		if [[ $count -ge $threads ]]; then
+			wait
+			count=0
+		fi
 	done
 fi
 
-# Add a newline between processor outputs and final messages
-echo ""
+wait
+
+# flac conversion loop
+if [ $ryconvertflac -gt 0 ]; then
+	for f in *.flac; do
+		flac_convert "$f" &
+		((count++))
+		# Try and find a way to start a new job as soon as an existing one ends
+		if [[ $count -ge $threads ]]; then
+			wait
+			count=0
+		fi
+	done
+fi
+
+wait
 
 # Show ALAC warning if applicale
 if echo "$ryconvertalacwarning" | grep 1; then
@@ -159,7 +182,7 @@ fi
 
 # Unset used environment variables
 # Reset text color to black
-echo "\033[0mCleaning up..."
+echo "\033[0m\nCleaning up..."
 unset ryconvertm4a
 unset ryconvertflac
 unset ryconvertm4acheck
